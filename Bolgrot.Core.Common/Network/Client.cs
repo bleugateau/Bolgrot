235a8a41dc980a723c8a005cd2eb33be08350e39
gateaubleu <34267388+gateaubleu@users.noreply.github.com>
2020-06-30 22:39:00 +0200

    [Core] Common - Adding network client

---
+++ Bolgrot.Core.Common/Network/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bolgrot.Core.Ankama.Protocol;
+using Newtonsoft.Json;
+
+namespace Bolgrot.Core.Common.Network
+{
+    public class Client
+    {
+        public string ContextId { get; }
+        
+        public ConcurrentDictionary<string, string> MessagesQueues { get; set; }
+
+        public Client(string contextId)
+        {
+            this.ContextId = contextId;
+            this.MessagesQueues = new ConcurrentDictionary<string, string>();
+        }
+
+        public void Send(NetworkMessage message)
+        {
+            this.MessagesQueues.TryAdd(message._messageType, $"4{JsonConvert.SerializeObject(message)}");
+        }
+    }
+}
