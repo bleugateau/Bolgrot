945a92534fc5ee3b437820a252597d976b9e6194
gateaubleu <34267388+gateaubleu@users.noreply.github.com>
2020-06-30 22:39:49 +0200

    [Server] Auth - Implementing client + frame intercepter OnMessageReceivedAsync()

---
+++ Bolgrot.Server.Auth/Frames/AuthenticationFrames.cs
@@ -0,0 +1,47 @@
+﻿using Bolgrot.Core.Ankama.Protocol.Messages;
+using Bolgrot.Core.Common.Managers.Frames;
+using Bolgrot.Core.Common.Network;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Bolgrot.Server.Auth.Frames
+{
+    public class AuthenticationFrames
+    {
+        
+        [InterceptCall("connecting")]
+        public void ConnectingCallFrame(Client client, JObject message)
+        {
+            client.Send(new ProtocolRequired(1594, 1594));
+            client.Send(new HelloConnectMessage("kbgvrb5aYZa&udoTr&~z3JACZDKTe&.F", new int[] { }));
+        }
+        
+        [InterceptCall("login")]
+        public void LoginCallFrame(Client client, JObject message)
+        {
+            if (!message.ContainsKey("data"))
+                return;
+
+            // var loginMessage = JsonConvert.DeserializeObject<LoginMessage>(message["data"].ToString());
+            //
+            //
+            // //find user in database and check
+            //
+            // //check token
+            // //client.Send(new IdentificationFailedMessage(0));
+            //
+            //
+            // //valid user
+            // client.Send(new CredentialsAcknowledgementMessage());
+            // client.Send(new IdentificationSuccessMessage("4638024884374416908", "Test", 141231, 0, true, "DELETE ?", 0, false, 1577995402000, true, "", ""));
+            //
+            // //IPC to world
+            // client.Send(new ServersListMessage(new GameServerInformations[]
+            // {
+            //     new GameServerInformations(128, 3, 0, true, 1, 1583348790936, "Arkanic PvP", 3)
+            // }));
+            //
+            // client.Dispose();
+        }
+    }
+}
--- Bolgrot.Server.Auth/Network/AuthServer.cs
+++ Bolgrot.Server.Auth/Network/AuthServer.cs
@@ -1,40 +1,70 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Bolgrot.Core.Common.Managers.Frames;
+using Bolgrot.Core.Common.Network;
 using EmbedIO.WebSockets;
 
 namespace Bolgrot.Server.Auth.Network
 {
     public class AuthServer : WebSocketModule
     {
+        
+        public ConcurrentDictionary<string, Client> Clients { get; set; }
+        
         public AuthServer(string urlPath) : base(urlPath, false)
         {
-        }
-
-        protected override Task OnMessageReceivedAsync(IWebSocketContext context, byte[] buffer, IWebSocketReceiveResult result)
-        {
-            Console.WriteLine($"Client {context.Id} sent => {System.Text.Encoding.UTF8.GetString(buffer)}");
-            
-            return Task.CompletedTask;
+            this.Clients = new ConcurrentDictionary<string, Client>();
         }
 
         protected override Task OnClientConnectedAsync(IWebSocketContext context)
         {
             Console.WriteLine($"Client {context.Id} connected");
+            this.Clients.TryAdd(context.Id, new Client(context.Id));
+
+            SendAsync(context, "0{\"sid\":\"h-Tc6sbvNVUqwrImAL-o\",\"upgrades\":[],\"pingInterval\":25000,\"pingTimeout\":5000}");
             
             return Task.CompletedTask;
         }
-        //
-        // protected override Task OnFrameReceivedAsync(IWebSocketContext context, byte[] buffer, IWebSocketReceiveResult result)
-        // {
-        //     Console.WriteLine($"Client {context.Id} frame sent => {System.Text.Encoding.UTF8.GetString(buffer)}");
-        //     
-        //     return Task.CompletedTask;
-        // }
-        //
+
+        protected override Task OnMessageReceivedAsync(IWebSocketContext context, byte[] buffer, IWebSocketReceiveResult result)
+        {
+            string message = System.Text.Encoding.UTF8.GetString(buffer);
+            
+            Console.WriteLine($"Client {context.Id} sent => {message}");
+            
+            if (!this.Clients.ContainsKey(context.Id))
+                return null;
+
+            this.Clients.TryGetValue(context.Id, out Client client);
+
+            FrameIntercepterManager.Intercept(client, message.Substring(1, message.Length - 1));
+
+            
+            foreach (var messageInQueue in client.MessagesQueues)
+            {
+                Console.WriteLine($"Send => {messageInQueue.Key}.");
+                SendAsync(context, messageInQueue.Value);
+            }
+            
+            
+            client.MessagesQueues.Clear();
+            
+            
+            
+
+            return Task.CompletedTask;
+            
+        }
 
         protected override Task OnClientDisconnectedAsync(IWebSocketContext context)
         {
+            if (!this.Clients.ContainsKey(context.Id))
+                return null;
+            
+            this.Clients.TryRemove(context.Id, out Client removedClient);
+            
             Console.WriteLine($"Client {context.Id} disconnected");
             
             return Task.CompletedTask;
