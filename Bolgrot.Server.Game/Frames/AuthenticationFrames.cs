107ea3a7c59253ee00ec37d46fbc44b59a77cbc3
gateaubleu <34267388+gateaubleu@users.noreply.github.com>
2020-08-12 13:12:43 +0200

    [Server] Game - AuthenticationFrame just prepare without check

--- Bolgrot.Server.Game/Frames/AuthenticationFrames.cs
+++ Bolgrot.Server.Game/Frames/AuthenticationFrames.cs
@@ -1,8 +1,12 @@
-﻿using Bolgrot.Core.Ankama.Protocol.Messages;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Bolgrot.Core.Ankama.Protocol.Messages;
 using Bolgrot.Core.Ankama.Protocol.SendMessages;
 using Bolgrot.Core.Ankama.Protocol.Types;
 using Bolgrot.Core.Common.Managers.Frames;
 using Bolgrot.Core.Common.Network;
+using Bolgrot.Core.Common.Repository;
 using Newtonsoft.Json.Linq;
 
 namespace Bolgrot.Server.Game.Frames
@@ -19,6 +23,9 @@ namespace Bolgrot.Server.Game.Frames
         [InterceptFrame("AuthenticationTicketMessage")]
         public void AuthenticationTicketMessageFrame(Client client, AuthenticationTicketMessage authenticationTicketMessage)
         {
+            
+            //verif du compte via IPC
+            
             client.Send(new AuthenticationTicketAcceptedMessage());
             client.Send(new AccountCapabilitiesMessage(141748286, true, 32767, 32767, 0, new int[] {}, new object()));
             client.Send(new TrustStatusMessage(true));
@@ -28,10 +35,25 @@ namespace Bolgrot.Server.Game.Frames
         public void CharactersListRequestMessageFrame(Client client,
             CharactersListRequestMessage charactersListRequestMessage)
         {
-            client.Send(new CharactersListMessage(true, new CharacterBaseInformations[]
+
+
+
+            var characters = Container.Instance().Resolve<ICharacterRepository>().Entities().Values.Where(x => x.AccountId == 1);
+            List<CharacterBaseInformations> characterBaseInformations = new List<CharacterBaseInformations>();
+
+
+            foreach (var character in characters)
             {
-                new CharacterBaseInformations(1, 200, "Ten", new EntityLook(1, new int[]{20, 2030}, new int[]{33536592, 50331596, 53058242, 69935891, 86612674}, new int[]{130}, new SubEntity[] {}), 2, false), 
-            }));
+                characterBaseInformations.Add(new CharacterBaseInformations(character.Id, 1, character.Name, character.EntityLook, character.Breed, character.Sex));
+            }
+            
+
+            // client.Send(new CharactersListMessage(true, new CharacterBaseInformations[]
+            // {
+            //     new CharacterBaseInformations(1, 200, "Ten", new EntityLook(1, new int[]{20, 2030}, new int[]{33536592, 50331596, 53058242, 69935891, 86612674}, new int[]{130}, new SubEntity[] {}), 2, false), 
+            // }));
+            
+            client.Send(new CharactersListMessage(true, characterBaseInformations.ToArray()));
         }
     }
 }
