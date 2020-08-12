686f08a686c1b0cec76f41a97a9704d4466a77f2
gateaubleu <34267388+gateaubleu@users.noreply.github.com>
2020-08-12 13:08:48 +0200

    [Core] Protocol - Adding CharacterCreationResult & AuthentificationFailed enum

---
+++ Bolgrot.Core.Ankama.Protocol/Enums/AuthentificationFailedEnum.cs
@@ -0,0 +1,19 @@
+﻿namespace Bolgrot.Core.Ankama.Protocol.Enums
+{
+    public enum AuthentificationFailedEnum
+    {
+        BAD_VERSION = 1,
+        WRONG_CREDENTIALS = 2,
+        BANNED = 3,
+        KICKED = 4,
+        IN_MAINTENANCE = 5,
+        TOO_MANY_ON_IP = 6,
+        TIME_OUT = 7,
+        BAD_IPRANGE = 8,
+        CREDENTIALS_RESET = 9,
+        EMAIL_UNVALIDATED = 10,
+        SERVICE_UNAVAILABLE = 53,
+        UNKNOWN_AUTH_ERROR = 99,
+        SPARE = 100
+    }
+}
---
+++ Bolgrot.Core.Ankama.Protocol/Enums/CharacterCreationResultEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace Bolgrot.Core.Ankama.Protocol.Enums
+{
+    public enum CharacterCreationResultEnum
+    {
+        OK = 0,
+        ERR_NO_REASON = 1,
+        ERR_INVALID_NAME = 2,
+        ERR_NAME_ALREADY_EXISTS = 3,
+        ERR_TOO_MANY_CHARACTERS = 4,
+        ERR_NOT_ALLOWED = 5,
+        ERR_NEW_PLAYER_NOT_ALLOWED = 6,
+        ERR_RESTRICED_ZONE = 7
+    }
+}
